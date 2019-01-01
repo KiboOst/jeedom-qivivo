dd089c3c849a9372c0aeee499f97548e1099e76f
KiboOst <KiboOst@users.noreply.github.com>
2019-01-01 16:37:43 +0100

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -29,14 +29,56 @@ function qivivo_update() {
 	}
 
 	if (config::byKey('client_id', 'qivivo') == '') {
-			config::save('client_id', $eqLogic->getConfiguration('client_id'), 'qivivo');
-			config::save('client_secret', $eqLogic->getConfiguration('client_secret'), 'qivivo');
-			config::save('username', $eqLogic->getConfiguration('username'), 'qivivo');
-			config::save('password', $eqLogic->getConfiguration('password'), 'qivivo');
-		}
+		config::save('client_id', $eqLogic->getConfiguration('client_id'), 'qivivo');
+		config::save('client_secret', $eqLogic->getConfiguration('client_secret'), 'qivivo');
+		config::save('username', $eqLogic->getConfiguration('username'), 'qivivo');
+		config::save('password', $eqLogic->getConfiguration('password'), 'qivivo');
+	}
+
+	$plugin = plugin::byId('qivivo');
+	$eqLogics = eqLogic::byType($plugin->getId());
+	foreach ($eqLogics as $eqLogic)
+	{
+		updateLogicalId($eqLogic, 'LastMsg', 'last_communication');
+		updateLogicalId($eqLogic, 'Firmware', 'firmware_version');
+
+		//module:
+		updateLogicalId($eqLogic, 'Ordre', 'module_order');
+		updateLogicalId($eqLogic, 'OrdreNum', 'order_num');
+		updateLogicalId($eqLogic, 'SetMode', 'set_order');
+		updateLogicalId($eqLogic, 'SetProgram', 'set_program');
+
+		//thermostat:
+		updateLogicalId($eqLogic, 'Consigne', 'temperature_order');
+		updateLogicalId($eqLogic, 'Chauffe', 'heating');
+		updateLogicalId($eqLogic, 'Temperature', 'temperature');
+		updateLogicalId($eqLogic, 'Humidité', 'humidity');
+		updateLogicalId($eqLogic, 'Presence', 'presence');
+		updateLogicalId($eqLogic, 'DernierePresence', 'last_presence');
+		updateLogicalId($eqLogic, 'IncOne', 'set_plus_one');
+		updateLogicalId($eqLogic, 'DecOne', 'set_minus_one');
+		updateLogicalId($eqLogic, 'SetDuréeOrdre ', 'set_time_order');
+		updateLogicalId($eqLogic, 'SetTemperature', 'set_temperature_order');
+		updateLogicalId($eqLogic, 'Annule_Ordre_Temp', 'cancel_time_order');
+		updateLogicalId($eqLogic, 'SetTempAbsence', 'set_absence_temperature');
+		updateLogicalId($eqLogic, 'SetTempNuit', 'set_night_temperature');
+		updateLogicalId($eqLogic, 'SetTempHorsGel', 'set_frost_temperature');
+		updateLogicalId($eqLogic, 'SetTempPres1', 'set_presence_temperature_1');
+		updateLogicalId($eqLogic, 'SetTempPres2', 'set_presence_temperature_2');
+		updateLogicalId($eqLogic, 'SetTempPres3', 'set_presence_temperature_3');
+		updateLogicalId($eqLogic, 'SetTempPres4', 'set_presence_temperature_4');
 	}
 
 }
+
+function updateLogicalId($eqLogic, $from, $to) {
+	$qivivoCmd = $eqLogic->getCmd(null, $from);
+	if (is_object($qivivoCmd)) {
+		$qivivoCmd->setLogicalId($to);
+		$qivivoCmd->save();
+	}
+}
+
 
 function qivivo_remove() {
 
