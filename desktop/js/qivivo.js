ec1f96d9a8c3f38c743c6e8e321f0f6cb1646a48
KiboOst <KiboOst@users.noreply.github.com>
2019-05-04 11:17:37 +0200

    Update qivivo.js

--- desktop/js/qivivo.js
+++ desktop/js/qivivo.js
@@ -664,6 +664,7 @@ $('.bt_downloadProgram').on('click',function(){ //called from modal!
         dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data))
         downloadAnchorNode = document.createElement('a')
         downloadAnchorNode.setAttribute("href",     dataStr)
+        downloadAnchorNode.setAttribute("target", "_blank")
         downloadAnchorNode.setAttribute("download", _filename)
         document.body.appendChild(downloadAnchorNode)
         downloadAnchorNode.click()
